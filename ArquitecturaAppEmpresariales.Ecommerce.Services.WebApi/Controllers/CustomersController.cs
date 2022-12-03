cdc035bc5c970298040d4aae73836da5b6718341
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2022-12-03 17:57:20 -0500

    update

---
+++ ArquitecturaAppEmpresariales.Ecommerce.Services.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,136 @@
+﻿using ArquitecturaAppEmpresariales.Ecommerce.Application.DTO;
+using ArquitecturaAppEmpresariales.Ecommerce.Application.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArquitecturaAppEmpresariales.Ecommerce.Services.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerApplication _customerApplication;
+
+        public CustomersController(ICustomerApplication customerApplication)
+        {
+            _customerApplication = customerApplication;
+        }
+
+        #region métodos Sincronos
+        [HttpPost]
+        public IActionResult Insert([FromBody] CustomersDto customersDto)
+        {
+            if (customersDto == null) return BadRequest();
+
+            var response = _customerApplication.Insert(customersDto);
+            if(response.IsSuccess) return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] CustomersDto customersDto)
+        {
+            if (customersDto == null) return BadRequest();
+
+            var response = _customerApplication.Update(customersDto);
+            if (response.IsSuccess) return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId")]
+        public IActionResult Delete(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId)) return BadRequest();
+
+            var response = _customerApplication.Delete(customerId);
+            if (response.IsSuccess) return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId")]
+        public IActionResult Get(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId)) return BadRequest();
+
+            var response = _customerApplication.Get(customerId);
+            if (response.IsSuccess) return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var response = _customerApplication.GetAll();
+            if (response.IsSuccess) return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+        #endregion
+
+        #region "Métodos Asincronos"
+
+        [HttpPost]
+        public async Task<IActionResult> InsertAsync([FromBody] CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+            var response = await _customerApplication.InsertAsync(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody] CustomersDto customersDto)
+        {
+            if (customersDto == null)
+                return BadRequest();
+            var response = await _customerApplication.UpdateAsync(customersDto);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId}")]
+        public async Task<IActionResult> DeleteAsync(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return BadRequest();
+            var response = await _customerApplication.DeleteAsync(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetAsync(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+                return BadRequest();
+            var response = await _customerApplication.GetAsync(customerId);
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var response = await _customerApplication.GetAllAsync();
+            if (response.IsSuccess)
+                return Ok(response);
+
+            return BadRequest(response.Message);
+        }
+
+        #endregion
+
+    }
+}
---
+++ ArquitecturaAppEmpresariales.Ecommerce.Services.WebApi/Program.cs
@@ -0,0 +1,40 @@
+using ArquitecturaAppEmpresariales.Application.Main;
+using ArquitecturaAppEmpresariales.Ecommerce.Application.Interface;
+using ArquitecturaAppEmpresariales.Ecommerce.Domain.Core;
+using ArquitecturaAppEmpresariales.Ecommerce.Domain.Interface;
+using ArquitecturaAppEmpresariales.Ecommerce.Infrastructure.Data;
+using ArquitecturaAppEmpresariales.Ecommerce.Infrastructure.Interface;
+using ArquitecturaAppEmpresariales.Ecommerce.Infrastructure.Repository;
+using ArquitecturaAppEmpresariales.Ecommerce.Transversal.Common;
+using ArquitecturaAppEmpresariales.Ecommerce.Transversal.Mapper;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//automapper y DI
+builder.Services.AddAutoMapper(x => x.AddProfile(new MappingsProfile()));
+builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+builder.Services.AddScoped<ICustomerApplication, CustomerApplication>();
+builder.Services.AddScoped<ICustomersDomain, CustomersDomain>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
