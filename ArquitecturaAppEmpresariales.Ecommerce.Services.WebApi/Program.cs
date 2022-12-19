43186475328186a715aa2f1918bec084fed92cfb
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2022-12-19 08:17:25 -0500

    update

--- ArquitecturaAppEmpresariales.Ecommerce.Services.WebApi/Program.cs
+++ ArquitecturaAppEmpresariales.Ecommerce.Services.WebApi/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddSwaggerModule();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
