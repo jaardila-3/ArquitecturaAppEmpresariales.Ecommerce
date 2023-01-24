20362b1d2fb74a944ef9900231163b38beb6a85d
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-01-23 22:19:08 -0500

    mstest sin resultados. falta entender y hacer que funcione

---
+++ ArquitecturaAppEmpresariales.Ecommerce.Application.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace ArquitecturaAppEmpresariales.Ecommerce.Application.Test
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(configurationBuilder => {
+                var integrationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+                configurationBuilder.AddConfiguration(integrationBuilder);
+            });
+        }
+    }
+}
--- ArquitecturaAppEmpresariales.Ecommerce.Application.Test/UsersApplicationTest.cs
+++ ArquitecturaAppEmpresariales.Ecommerce.Application.Test/UsersApplicationTest.cs
@@ -1,3 +1,8 @@
+using ArquitecturaAppEmpresariales.Ecommerce.Application.Interface;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace ArquitecturaAppEmpresariales.Ecommerce.Application.Test
 {
     /// <summary>
@@ -6,45 +11,71 @@ namespace ArquitecturaAppEmpresariales.Ecommerce.Application.Test
     [TestClass]
     public class UsersApplicationTest
     {
+        private static WebApplicationFactory<Program>? _factory = null;
+        private static IServiceScopeFactory? _scopeFactory = null;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext _)
+        {
+            _factory = new CustomWebApplicationFactory();
+            _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
+        }
+
         [TestMethod]
         public void Authenticate_CuandoNoSeEnvianParametros_RetornarMensajeErrorValidacion()
         {
+            using var scope = _scopeFactory!.CreateScope();
+            var context = scope.ServiceProvider.GetService<IUsersApplication>();
+
             // Arrange
             var userName = string.Empty;
             var password = string.Empty;
             var expected = "Errores de Validación";
 
             // Act            
-            //var result = context.Authenticate(userName, password);
-            //var actual = result.Message;
+            var result = context!.Authenticate(userName, password);
+            var actual = result.Message;
 
             // Assert
-            //Assert.AreEqual(expected, actual);
-
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Authenticate_CuandoSeEnvianParametrosCorrectos_RetornarMensajeExito()
         {
+            using var scope = _scopeFactory!.CreateScope();
+            var context = scope.ServiceProvider.GetService<IUsersApplication>();
+
             // Arrange
-            var userName = "ALEX";
-            var password = "123456";
+            var userName = "jorge";
+            var password = "2404";
             var expected = "Autenticación Exitosa!!!";
 
-            //Act
-            //Assert
+            // Act
+            var result = context!.Authenticate(userName, password);
+            var actual = result.Message;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         public void Authenticate_CuandoSeEnvianParametrosIncorrectos_RetornarMensajeUsuarioNoExiste()
         {
+            using var scope = _scopeFactory!.CreateScope();
+            var context = scope.ServiceProvider.GetService<IUsersApplication>();
+
             // Arrange
             var userName = "ALEX";
             var password = "123456899";
             var expected = "Usuario no existe";
 
-            //Act
-            //Assert
+            // Act
+            var result = context!.Authenticate(userName, password);
+            var actual = result.Message;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
